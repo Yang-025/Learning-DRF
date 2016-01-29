7caa66adfa0e2d533b7a07a71e47da9cfa143af2
winsome_yang <kkadso@gmail.com>
2016-01-29 21:27:05 +0800

    Tutorial 5: Relationships & Hyperlinked APIs

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,18 +4,39 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+# old method : 使用ModelSerializer
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'snippets')
+#
+# # new method
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#     # ReadOnlyField 也可以改成CharField(read_only=True)
+#     # owner = serializers.CharField(read_only=True,source='owner.username')
+#     class Meta:
+#         model = Snippet
+#         fields = ('id', 'title', 'code', 'linenos', 'language', 'style','owner')
+
+
+# new method 使用 HyperlinkedModelSerializer 來達到表之間的互聯
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    # 確保回傳格式是html
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
-
-# new method
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    # ReadOnlyField 也可以改成CharField(read_only=True)
-    # owner = serializers.CharField(read_only=True,source='owner.username')
-    class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style','owner')
+        fields = ('url', 'username', 'snippets')
--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,18 +1,29 @@
-#! -*- encoding: utf-8 -*-
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-from django.conf.urls import include
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    url(r'^$', views.api_root),
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
 
-urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    # 登入 http://127.0.0.1:8000/api-auth/login/ 頁面
+# Login and logout views for the browsable API
+urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
-                           namespace='rest_framework')),
+                               namespace='rest_framework')),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
